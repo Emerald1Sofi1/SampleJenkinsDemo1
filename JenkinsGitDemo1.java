8895a39b05e43bf875b1475b17b6c4a8de0df4da
Emerald1Sofi1 <97340648+Emerald1Sofi1@users.noreply.github.com>
2022-01-09 13:45:00 +0530

    Update JenkinsGitDemo1.java

--- JenkinsGitDemo1.java
+++ JenkinsGitDemo1.java
@@ -1,6 +1,6 @@
 public class JenkinsGitDemo1{
 public static void main(String[] args){
 System.out.println("This is  Jenkins GIT integration");
-  System.out.println("This is  Jenkins GIT integration newly added for push test final");
+  System.out.println("This is  Jenkins GIT integration newly added for push test fnal");
 }
 }
